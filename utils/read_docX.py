81a85aa5ab3c7730d0a7819c0a9f02d57e88876c
nicesoul <nicesoul.beyourself@gmail.com>
2021-09-09 11:35:19 +0200

    docX parser update = LINKS
    parsing links with a parser. They are inserted not in place but at the top of document

--- utils/read_docX.py
+++ utils/read_docX.py
@@ -4,18 +4,27 @@
 
 # import re
 import docx
-
+from docx.opc.constants import RELATIONSHIP_TYPE as RT
 
 # working if run from utils folder
 
 def get_full_text(filename):
     doc = docx.Document(filename)
     full_text = []
+
+    # parsing links
+    rels = doc.part.rels
+    for rel in rels:
+        if rels[rel].reltype == RT.HYPERLINK:
+            full_text.append(rels[rel]._target)
+    print('links found:', len(full_text))
+
     # parsing plain text paragraphs
     prgrphs = doc.paragraphs
     print('paragraphs found:', len(prgrphs))
     for paragraph in prgrphs:
         full_text.append(paragraph.text)
+
     # parsing text in tables
     tables = doc.tables
     print('tables found: ', len(tables))
