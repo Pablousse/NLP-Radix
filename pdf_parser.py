c812ac0775169664c1e9099e79d54bf7cd769848
Pablousse <durieux.arnaud@hotmail.com>
2021-09-10 09:03:24 +0200

    improved the section parsing

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,6 +1,7 @@
-import collections
 import fitz
 import pandas as pd
+import os
+import re
 # import spacy
 
 
@@ -23,6 +24,7 @@ def extract_info_from_page(page_number, min_paragraph_size_adjusted, doc):
         size = ""
         font = ""
         color = ""
+        bold_text = ""
         number = block["number"]
         is_line_bold = 0
         if 'lines' in block.keys():
@@ -33,17 +35,20 @@ def extract_info_from_page(page_number, min_paragraph_size_adjusted, doc):
                     if span["text"].strip() != "":
                         size += str(span["size"]) if size == "" else "," + str(span["size"])
                         font += str(span["font"]) if font == "" else "," + str(span["font"])
+                        if "bold" in span["font"].lower():
+                            bold_text += str(span["text"])
                         color += str(span["color"]) if color == "" else "," + str(span["color"])
             if all( "bold" in i.lower() for i in font.split(",") ):
                 is_line_bold = 1
-        font_info.append([number, size, font, color, is_line_bold])
+        font_info.append([number, size, font, color, is_line_bold, bold_text])
 
-    df_info = pd.DataFrame(font_info, columns=["block_number", "size", "font", "color", "is_line_bold"])
+    df_info = pd.DataFrame(font_info, columns=["block_number", "size", "font", "color", "is_line_bold", "bold_text"])
     df_block = df_block.merge(df_info, on="block_number")
     df_block["is_title"] = 0
-    df_block.loc[(df_block['text'].str.split().str.len() < 4) & (df_block["is_line_bold"] == 1), 'is_title'] = 1
+    df_block["bold_text"] = df_block["bold_text"].str.replace("-", " ").str.replace(":", " ").str.strip()
+    df_block.loc[(df_block['text'].str.replace("-", " ").str.replace(":", " ").str.strip().str.split().str.len() < 6) & (df_block["bold_text"] != ""), 'is_title'] = 1
     x = df_block[df_block.index == 5]["font"]
-    pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_rows', None)
     # print(df_block)
 
 
@@ -53,10 +58,10 @@ def extract_info_from_page(page_number, min_paragraph_size_adjusted, doc):
 
     #
 
-    df_block["next_y0"] = df_block["y0"].shift(-1)
-    df_block["next_x0"] = df_block["x0"].shift(-1)
-    df_block["previous_x0"] = df_block["x0"].shift(+1)
-    df_block["diff"] = df_block["next_y0"] - df_block["y1"]
+    # df_block["next_y0"] = df_block["y0"].shift(-1)
+    # df_block["next_x0"] = df_block["x0"].shift(-1)
+    # df_block["previous_x0"] = df_block["x0"].shift(+1)
+    # df_block["diff"] = df_block["next_y0"] - df_block["y1"]
 
     new_list_block = []
     text = ""
@@ -64,24 +69,24 @@ def extract_info_from_page(page_number, min_paragraph_size_adjusted, doc):
 
     # print(min_paragraph_size_adjusted)
 
-    for index, row in df_block.iterrows():
-        if row["diff"] > min_paragraph_size_adjusted or row["diff"] < 0:
-            row["text"] = text + " " + row["text"]
-            row["x0"] = x0 if x0 != "" else row["x0"]
-            new_list_block.append(row)
-            text = ""
-            x0 = ""
-        else:
-            text += row["text"]
-            if x0 == "":
-                x0 = row["x0"]
+    # for index, row in df_block.iterrows():
+    #     if row["diff"] > min_paragraph_size_adjusted or row["diff"] < 0:
+    #         row["text"] = text + " " + row["text"]
+    #         row["x0"] = x0 if x0 != "" else row["x0"]
+    #         new_list_block.append(row)
+    #         text = ""
+    #         x0 = ""
+    #     else:
+    #         text += row["text"]
+    #         if x0 == "":
+    #             x0 = row["x0"]
 
-    new_df_block = pd.DataFrame(new_list_block)
+    # new_df_block = pd.DataFrame(new_list_block)
 
-    new_df_block["next_x0"] = new_df_block["x0"].shift(-1)
-    new_df_block["previous_x0"] = new_df_block["x0"].shift(+1)
+    # new_df_block["next_x0"] = new_df_block["x0"].shift(-1)
+    # new_df_block["previous_x0"] = new_df_block["x0"].shift(+1)
 
-    return pd.DataFrame(new_df_block)
+    return pd.DataFrame(df_block)
 
 
 def extract_info_from_pdf(pdf_path, percentage_adjustment):
@@ -130,28 +135,80 @@ def extract_info_from_pdf(pdf_path, percentage_adjustment):
 
     return df_informations
 
+pdf_array = []
 
-df = extract_info_from_pdf("assets/pdf/12.pdf", 0.2)
-# print(df)
-# print(df.iloc[7]["text"])
+# for filename in os.scandir("assets/pdf/"):
+#     if filename.is_file():
+#         print(filename.path)
+#         print(int(re.search(r'\d+', filename.path).group()))
+        
 
-objective = ["objective", "objectives", "synopsis"]
-skills = ["skill", "computer", "certification", "technical"]
-education = ["educational", "qualification", "academic", "scholastic"]
-experience = ["experience", "project", "employment", "internship"]
-hobbies = ["hobbies", "curricular", "co-curricular", "activity", "activities"]
-personnal = ["personnal", "personality", "personal", "about me"]
-other = ["strength", "strenth", "strenght", "declaration", "declairation", "language", "achievements"]
-category_list = [objective, skills, education, experience, hobbies, personnal, other]
-df["category"] = ""
-# print(df)
-for categories in category_list:
-    for category in categories:
-        print(category)
-        df.at[df[((df['text'].str.contains(category, case=False)) | (df['text'].str.contains((category+"s"), case=False))) & (df['is_line_bold'] == 1)].index, "category"] = category
+# for pdf_number in range(122,123):
+#     if 1==1:
+#         path = "assets/pdf/" + str(pdf_number) + ".pdf"
+#         print(path)
+#         df = extract_info_from_pdf(path, 0.2)
+        
+for filename in os.scandir("assets/pdf/"):
+    if filename.is_file() and filename.path:
+        
+        print(filename.path)
+        pdf_number = int(re.search(r'\d+', filename.path).group())
+        df = extract_info_from_pdf(filename.path, 0.2)
 
-print(df)
+        objective = ["objective", "synopsis"]
+        personnal = ["personnal", "personality", "personal", "about me", "address"]
+        education = ["educational", "qualification", "academic", "scholastic", "education"]
+        experience = ["experience", "project", "employment", "internship", "training"]
+        hobbies = ["hobbies", "curricular", "activity", "activities"]
+        skills = ["skill", "computer", "certification", "technical"]
+        other = ["strength", "strenth", "strenght", "declaration", "declairation", "achievements"]
+        category_list = {"other" : other, "personnal" : personnal, "hobbies" : hobbies, "objective" : objective, "skills" : skills, "education" : education, "experience" : experience}
+        df["category"] = ""
+        # print(df)
+        for key,value in category_list.items():
+            for category in value:
+                df.at[df[((df["bold_text"].str.contains(category, case=False)) | (df["bold_text"].str.contains((category+"s"), case=False)))].index, "category"] = key
 
+        pd.set_option('display.max_rows', None)
+        # print(df[["text", "is_line_bold", "category", "bold_text"]])
+
+        new_df_array = []
+        text = ""
+        category = ""
+        for index, row  in df.iterrows():
+            if text != "" and row["category"] != "":
+                new_df_array.append([text, category])
+                text = ""
+                category = ""
+            text += row["text"]
+            if row["category"] != "":
+                category += row["category"]
+        new_df_array.append([text, category])
+
+        count_subject = pd.DataFrame(new_df_array, columns=["text", "subject"])
+
+        # pdf_name, 
+        pdf_df = count_subject['subject'].value_counts()
+        # print(df[["text", "bold_text", "is_title", "color", "category"]])
+        pdf_array.append([pdf_number, pdf_df.get('', default=0),pdf_df.get('experience', default=0),pdf_df.get('objective', default=0),pdf_df.get('personnal', default=0),pdf_df.get('education', default=0),pdf_df.get('hobbies', default=0),pdf_df.get('skills', default=0),pdf_df.get('other', default=0)])
+
+pdf_df = pd.DataFrame(pdf_array, columns=["pdf_number","", "experience", "objective", "personnal", "education", "hobbies", "skills", "other"])
+pdf_df = pdf_df.set_index('pdf_number')
+pdf_df["count"] =  pdf_df.sum(axis=1)
+print(pdf_df)
+pdf_df.to_csv('out.csv')  
+    # print(pdf_number, pdf_df.get('', default=0),pdf_df.get('experience', default=0),pdf_df.get('objective', default=0),pdf_df.get('personnal', default=0),pdf_df.get('education', default=0),pdf_df.get('hobbies', default=0),pdf_df.get('skills', default=0),pdf_df.get('other', default=0))
+# print(pdf_df.loc["experience"])
+# print(pdf_df.loc["objective"])
+# print(pdf_df.loc["personnal"])
+# print(pdf_df.loc["education"])
+# print(pdf_df.loc["hobbies"])
+# print(pdf_df.loc["skills"])
+# print(pdf_df.loc["other"])
+# print(pdf_df.loc[""])
+
+# print(pdf_df.get('', default=0))
 # nlp = spacy.load('en_core_web_sm')
 
 # doc = nlp("Educational")
